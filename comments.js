a615f383d737edcd0024e90f997f2f4da6de9628
shreyash4real <145307465+shreyash4real@users.noreply.github.com>
2023-09-25 16:00:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,69 @@
 //create web server
-var express = require('express');
-var router = express.Router();
-var comments = require('../controllers/comments');
-var auth = require('../controllers/auth');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'comments';
+const client = new MongoClient(url);
 
-// GET
-router.get('/', comments.list);
-router.get('/:id', comments.get);
-router.get('/user/:id', comments.listByUser);
-router.get('/post/:id', comments.listByPost);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// POST
-router.post('/', auth.isAuthenticated, comments.create);
+//connect to database
+let db;
+client.connect(function(err) {
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
+});
 
-// PUT
-router.put('/:id', auth.isAuthenticated, comments.update);
+//get all comments
+app.get('/comments', function(req, res) {
+    db.collection('comments').find({}).toArray(function(err, docs) {
+        console.log(docs);
+        res.send(docs);
+    });
+});
 
-// DELETE
-router.delete('/:id', auth.isAuthenticated, comments.delete);
+//get all comments on a post
+app.get('/comments/:post', function(req, res) {
+    console.log(req.params.post);
+    db.collection('comments').find({post: req.params.post}).toArray(function(err, docs) {
+        console.log(docs);
+        res.send(docs);
+    });
+});
 
-module.exports = router;
+//add new comment
+app.post('/comments', function(req, res) {
+    console.log(req.body);
+    db.collection('comments').insertOne(req.body, function(err, result) {
+        console.log("Inserted 1 document into the collection");
+        res.send(result);
+    });
+});
+
+//delete comment
+app.delete('/comments/:id', function(req, res) {
+    console.log(req.params.id);
+    db.collection('comments').deleteOne({_id: req.params.id}, function(err, result) {
+        console.log("Deleted 1 document from the collection");
+        res.send(result);
+    });
+});
+
+//update comment
+app.put('/comments/:id', function(req, res) {
+    console.log(req.body);
+    db.collection('comments').updateOne({_id: req.params.id}, {$set: req.body}, function(err, result) {
+        console.log("Updated 1 document in the collection");
+        res.send(result);
+    });
+});
+
+//start server
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+}
